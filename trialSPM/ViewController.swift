a80a663af5bef474c0d4942ddef7943507ea2bfc
Kirti S <shilwantkirti@gmail.com>
2021-04-05 10:28:43 +0530

    Initial Commit

---
+++ trialSPM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trialSPM
+//
+//  Created by k on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
